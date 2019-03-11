5b7ea180fab007d39af7c82bba1cf088f4820786
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-03-11 13:50:18 +0530

    Update port :construction:

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use('/graphql', graphQLHttp({
     graphiql: true
 }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log(`Listening on localhost:${PORT}/graphql`);
 });
